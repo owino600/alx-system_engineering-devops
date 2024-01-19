14a19adefe7a7360b1909e7dcd01632c43a04906
owino600 <owinodaniel600@gmail.com>
2024-01-19 12:05:36 +0300

    killmenow

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,13 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 package {'flask':
+<<<<<<< HEAD
   ensure   => '2.1.0',
   provider => 'pip3',
+=======
+	ensure   => '2.1.0',
+		 provider => 'pip3',
+>>>>>>> 7b9dfa7 (killmenow)
 }
 # Install werkzeug
 package {
