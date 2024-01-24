6c501ed1af21206c4caee7e6e8f36de08c1ebbf9
owino600 <owinodaniel600@gmail.com>
2024-01-24 17:23:03 +0300

    1-install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,10 +4,8 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-# Install werkzeug
-package {
-  'Werkzeug':
+
+package { 'Werkzeug':
   ensure => '2.1.1',
   provider => 'pip3',
-  require => Package['Flask']
 }
