7ba5e1e0a442d208930495a56275313f91175023
owino600 <owinodaniel600@gmail.com>
2024-01-24 17:20:22 +0300

    1-install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,8 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 package {'flask':
-<<<<<<< HEAD
   ensure   => '2.1.0',
   provider => 'pip3',
-=======
-	ensure   => '2.1.0',
-		 provider => 'pip3',
->>>>>>> 7b9dfa7 (killmenow)
 }
 # Install werkzeug
 package {
