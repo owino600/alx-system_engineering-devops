996a421755e6e3f63aa65c6dd215ccbfe1aaf096
owino600 <owinodaniel600@gmail.com>
2024-03-05 18:45:03 +0300

    .\1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
-def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-    """
-    try:
-        # Set a custom User-Agent to avoid Too Many Requests errors
-        headers = {'User-Agent': 'MyRedditBot'}
-        
-        # Make an API request to get hot posts
-        url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        
-        # Extract and print post titles
-        for post in data['data']['children']:
-            title = post['data']['title']
-            print(title)
-    except (KeyError, requests.RequestException):
-        # Invalid subreddit or other error occurred
-        print(None)
 
-# Example usage:
-subreddit_name = 'learnprogramming'
-top_ten(subreddit_name)
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
