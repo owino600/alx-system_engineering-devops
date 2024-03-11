d0c10c656645ddc1228bdfb1043419acd0311cb3
owino600 <owinodaniel600@gmail.com>
2024-03-11 18:39:28 +0300

    .\100-count.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,6 @@
 """Function to print hot posts on a given Reddit subreddit."""
 import requests
 
-
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
@@ -14,7 +13,7 @@ def top_ten(subreddit):
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code != 200:
         print("None")
         return
     results = response.json().get("data")
