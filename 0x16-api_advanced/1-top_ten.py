6b646f8cadea365ba5ab1e56735f1de8512482fc
owino600 <owinodaniel600@gmail.com>
2024-03-11 18:49:36 +0300

    .\1-main.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 """Function to print hot posts on a given Reddit subreddit."""
+
 import requests
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    params = {'after': after} if after else {}
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    # If the subreddit is not valid, return None
     if response.status_code != 200:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+        return None
+
+    data = response.json()
+    hot_list.extend([post['data']['title'] for post in data['data']['children']])
+
+    # If there are more posts, recurse with the 'after' parameter
+    after = data['data']['after']
+    if after:
+        return recurse(subreddit, hot_list, after)
+
+    return hot_list
